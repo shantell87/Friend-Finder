95e44962ca3dcad810fe463d479cd86abc60221f
Shantell Davis <shantell87@gmail.com>
2019-07-28 00:14:04 -0700

    setup file structure

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
