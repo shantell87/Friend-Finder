2971a5c7aff1845f0185709a07743078ec79fb9a
Shantell Davis <shantell87@gmail.com>
2019-07-28 22:52:29 -0700

    updated home and survey html pages

---
+++ app/routing/htmlRoute.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = function(app){
+    app.get("/survey", (req, res) => {
+        res.sendFile(path.join(__dirname, "/../public/survey"))
+    });
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "/../public/home"))
+    });
+}
